a31cd62a69282cf4522a9cd6ec8d22e867550cba
saif <you@example.com>
2023-07-30 21:29:36 +0600

    [loginform] login form is added.

---
+++ src/LoginApp.js
@@ -0,0 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+
+function LoginForm() {
+
+    return (
+        <Container>
+            <Form>
+                <Row className="sm-3">
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                </Row>
+
+                <Row className="sm-3">
+                    <Form.Group as={Col} controlId="formGridPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                </Row>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+    );
+}
+
+export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './LoginApp'
 import reportWebVitals from './reportWebVitals';
+import LoginForm from './LoginApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoginForm />
   </React.StrictMode>
 );
 
