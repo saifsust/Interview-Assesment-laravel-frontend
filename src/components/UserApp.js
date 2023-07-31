5b472ce07a0dba36de66e83ddcf91f8f0e113647
saif <you@example.com>
2023-07-31 21:51:35 +0600

    [Ajax] API call test is done.

--- src/components/UserApp.js
+++ src/components/UserApp.js
@@ -1,7 +1,26 @@
 import { Container, Table } from "react-bootstrap";
-
+import React, { useState, useEffect } from 'react';
 
 export default function UserApp() {
+
+    const getUsers = async () => {
+        await fetch('http://localhost:8000/api/user/all', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
+
+    getUsers();
+
     return (
         <Container>
             <Table striped="columns">
