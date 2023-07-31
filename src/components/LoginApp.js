db044c1492e5075833f605e5d09e4b0a9f08e82e
saif <you@example.com>
2023-07-31 21:20:42 +0600

    [Book Meta] form submit navigation is done.

--- src/components/LoginApp.js
+++ src/components/LoginApp.js
@@ -1,15 +1,19 @@
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+import { Container, Button, Form, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function LoginApp() {
 
+    const navigation = useNavigate();
+
+    const handleSubmit = (data) => {
+        console.log(data);
+        navigation("/user")
+    }
+
     return (
         <Container>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Row className="sm-3">
                     <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
